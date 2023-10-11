6a0bc103e288a1f9fb88cfe04337318b00ae0d04
Abdeslam Boutaarourt <abdeslam.boutaarourt@student.ehb.be>
2023-10-11 10:15:29 +0200

    server and webcam component

--- WebRTC/WebRTC/src/component/WebcamComponent.tsx
+++ WebRTC/WebRTC/src/component/WebcamComponent.tsx
@@ -1,57 +1,61 @@
-import { useEffect , useState } from 'react';
-import './css/Webcam.css';
-const WebcamComponent = () => {
-    
+import { useEffect, useState } from "react";
+import "./css/Webcam.css";
 
-    const [mediaStream, setMediaStream] = useState(null);
+interface MediaStreamConstraints {
+  video: boolean;
+  audio: boolean;
+}
 
-    useEffect(() => {
-        const startButton = document.querySelector('#start');
-        const stopButton = document.querySelector('#stop');
-        const videoElement = document.getElementById('emitter-video');
+const WebcamComponent: React.FC = () => {
+  const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
 
-        startButton.addEventListener('click', async () => {
-            const constraints = {
-                'video': true,
-                'audio': true
-            };
+  useEffect(() => {
+    const startButton = document.querySelector("#start") as HTMLButtonElement;
+    const stopButton = document.querySelector("#stop") as HTMLButtonElement;
+    const videoElement = document.getElementById("emitter-video") as HTMLVideoElement;
 
-            try {
-                const stream = await navigator.mediaDevices.getUserMedia(constraints);
-                console.log('Got MediaStream:', stream);
-                videoElement.srcObject = stream;
-                videoElement.play();
-                setMediaStream(stream);
-            } catch (error) {
-                console.error('Error accessing media devices.', error);
-            }
-          
+    startButton.addEventListener("click", async () => {
+      const constraints: MediaStreamConstraints = {
+        video: true,
+        audio: true,
+      };
+
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        console.log("Got MediaStream:", stream);
+
+        videoElement.srcObject = stream;
+
+        videoElement.addEventListener("canplay", () => {
+          videoElement.play();
         });
-
-        stopButton.addEventListener('click', () => {
-            if (mediaStream) {
-                const tracks = mediaStream.getTracks();
-                tracks.forEach(track => track.stop());
-                videoElement.srcObject = null;
-                setMediaStream(null);
-
-            }
-        });
+        setMediaStream(stream);
+      } catch (error) {
+        console.error("Error accessing media devices.", error);
+      }
 
 
-    }, [mediaStream]);
+    });
 
+    stopButton.addEventListener("click", () => {
+      if (mediaStream) {
+        const tracks = mediaStream.getTracks();
+        tracks.forEach((track) => track.stop());
+        videoElement.srcObject = null;
+        setMediaStream(null);
+      }
+    });
+  }, [mediaStream]);
 
-    return (
-        <div id='video'>
-            <div>
-            <button id="start">Start</button>
-            <button id="stop">Stop</button>
-</div>
-            <video id="emitter-video" width="100%" height="400px"></video>
-
-        </div>
-    );
+  return (
+    <div id="video">
+      <div>
+        <button id="start">Start</button>
+        <button id="stop">Stop</button>
+      </div>
+      <video id="emitter-video" width="100%" height="400px"></video>
+    </div>
+  );
 };
 
 export default WebcamComponent;
