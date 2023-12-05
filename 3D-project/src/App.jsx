840677d5075f029d78c8bdec33e4dcbef750972c
Abdeslam Boutaarourt <abdeslam.boutaarourt@student.ehb.be>
2023-12-05 20:16:20 +0100

    loaded 3d model with gltfjsx

--- 3D-project/src/App.jsx
+++ 3D-project/src/App.jsx
@@ -1,14 +1,20 @@
-import './App.css'
+import "./App.css";
+import { Suspense } from "react";
+
+import { Canvas } from "@react-three/fiber";
+import Goingmerry from "../models/Goingmerry";
 
 function App() {
-
   return (
     <>
-    <div className='Outerwrapper'>
-      <h1>hi</h1>
-    </div>
+        <Canvas style={{ height: "100vh" }} camera={{ position: [10, 10, 10] }}>
+          <ambientLight intensity={2}/>
+            <Suspense fallback={null}>
+              <Goingmerry />
+            </Suspense>
+        </Canvas>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
